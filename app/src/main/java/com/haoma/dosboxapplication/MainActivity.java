c6f6eaf622b8e15ff5d1d439cbf4595a7dd9338f
XINHAO_HAN <1500252978@qq.com.com>
2019-12-30 12:11:38 +0800

    XINHAO_HAN_Dos_Box

---
+++ app/src/main/java/com/haoma/dosboxapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.haoma.dosboxapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.fishstix.dosboxfree.DBMain;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, DBMain.class));
+    }
+}
